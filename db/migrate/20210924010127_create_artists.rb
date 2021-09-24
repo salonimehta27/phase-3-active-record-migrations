356ff0dfd3ca6dc0063ccad319b8dd6696e0567a
Saloni <salonimehta2127@gmail.com>
2021-09-23 21:39:18 -0400

    Completed

---
+++ db/migrate/20210924010127_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :artists do |t|
+      #t.string is a method that takes a symbol as an argument and creates a column
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
